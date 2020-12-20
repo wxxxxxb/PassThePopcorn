b5a865edb4692c0435aa4cac665553a645ebcb02
WxB <i@wxb.cx>
2020-12-20 22:24:30 +0100

    Add function to get info of a torrent

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,13 @@ import Connection from './connection'
 import Movie from './objects/movie'
 import Torrent from './objects/torrent'
 
-interface SearchResult {
+interface MovieSearchResult {
   Movies: Array<Record<string, any>>
+}
+
+interface TorrentSearchResult {
+  GroupId: number
+  Torrents: Array<Record<string, any>>
 }
 
 class Api {
@@ -22,7 +27,7 @@ class Api {
   async search(query: string): Promise<Movie[]> {
     const response = ((await this.connection.getJSON('torrents.php', {
       searchParams: { searchstr: query, jsontrumpable: 1, jsonreports: 1 },
-    })) as unknown) as SearchResult
+    })) as unknown) as MovieSearchResult
     return response.Movies.map(
       (movieData: Record<string, unknown>) => new Movie(movieData),
     )
@@ -30,6 +35,34 @@ class Api {
 
   async searchByImdbId(id: string): Promise<Movie> {
     return (await this.search(id))[0]
+  }
+
+  async getTorrentInfo(torrentId: number): Promise<any> {
+    let response
+    try {
+      response = ((await this.connection.getJSON('torrents.php', {
+        searchParams: {
+          torrentid: torrentId,
+        },
+      })) as unknown) as TorrentSearchResult
+    } catch (err) {
+      // if id is not found, ptp gives a 403
+      // we do an extra check for the body to make sure it's not the credentials
+      if (
+        err.response.statusCode === 403 &&
+        err.response.body ===
+          'This page is not allowed to be accessed by the API\n'
+      ) {
+        return false
+      }
+      throw err
+    }
+
+    console.log(response)
+    const torrentData = response.Torrents.find(
+      ({ Id }) => Number(Id) === torrentId,
+    )
+    return new Torrent(Number(response.GroupId), torrentData)
   }
 
   async getDescriptionForTorrent(torrent: Torrent): Promise<string> {
